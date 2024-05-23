2178f7ae9b8590968cbd203f0847d8be0656f002
john costello <jcostello711@gmail.com>
2024-05-22 23:57:02 -0400

    backend basic functionality tested via Postman and working. processing being simulated for now.

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const axios = require('axios');
+const fs = require('fs');
+const path = require('node:path');
+
+const app = express();
+const port = 3000;
+
+const upload = multer({ dest: 'uploads/' });
+
+app.use(bodyParser.raw({ type: 'application/octet-stream', limit: '35mb' }));
+app.use(express.static('public'));
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    const filePath = path.join(__dirname, 'uploads', 'pkROM.z64');
+    fs.writeFileSync(filePath, req.body);
+    const fileBuffer = fs.readFileSync(filePath);
+  
+  axios({
+    method: 'post',
+    url: 'http://localhost:5000/process',
+    data: fileBuffer,
+    responseType: 'stream',
+    headers: {
+      'Content-Type': 'application/octet-stream'
+    }
+  })
+  .then(response => {
+    const outputFilePath = path.join(__dirname, 'downloads', 'randomizedROM.z64');
+    const writer = fs.createWriteStream(outputFilePath);
+    
+    response.data.pipe(writer);
+    
+    writer.on('finish', () => {
+      res.download(outputFilePath, err => {
+        if (err) {
+          console.error(err);
+        }
+        fs.unlinkSync(filePath);
+        fs.unlinkSync(outputFilePath);
+      });
+    });
+  })
+  .catch(error => {
+    console.error(error);
+    res.status(500).send('Error processing file');
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
